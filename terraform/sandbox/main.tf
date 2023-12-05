4576c13f37c527b8b8525ea3d41bc40acb30761a
John Cheng <jcheng@natera.com>
2023-12-05 06:26:00 -0800

    adding s3 pub access block

---
+++ terraform/sandbox/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_account_public_access_block" "s3_block" {
+	block_public_acls = true
+	block_public_policy = true
+	ignore_public_acls = true
+	restrict_public_buckets = true
+
+
+
+}
